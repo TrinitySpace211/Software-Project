c3ad66bef6d9c0ad884743817e8104f1bd4feabf
Nikita-Si777 <nikksi777@gmail.com>
2026-05-29 18:21:15 +0200

    feat: Crossbow mit Button bauen

--- Assets/Scripts/ScriptsNikita/CanvasInteract.cs
+++ Assets/Scripts/ScriptsNikita/CanvasInteract.cs
@@ -19,28 +19,4 @@ public class CanvasInteract : MonoBehaviour
         float y = Mathf.Sin(Time.time * floatSpeed) * floatHeight;
         transform.localPosition = startPos + new Vector3(0, y, 0);
     }
-
-
-
-
-
-
-    /*public float speed = 3f;
-    public float amount = 0.15f;
-
-    private Vector3 startScale;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        startScale = transform.localScale;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float scale = 1f + Mathf.Sin(Time.time * speed) * amount;
-        transform.localScale = startScale * scale;
-    }
-    */
 }
--- Assets/Scripts/ScriptsNikita/NPCDialog.cs
+++ Assets/Scripts/ScriptsNikita/NPCDialog.cs
@@ -1,27 +1,65 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class NPCDialog : MonoBehaviour {
     public GameObject dialogPanel;
+    public GameObject functionsPanel;
+
+    public GameObject towerPrefab;
+    public Transform towerSpawnPoint;
+
+    public  void start() {
+
+        // Panels kurz aktivieren und dann wieder deaktivieren, um "Ruckler" beim Wechseln der Panels zu vermeiden.
+        dialogPanel.SetActive(true);
+        functionsPanel.SetActive(true);
+
+        Canvas.ForceUpdateCanvases();
+
+        dialogPanel.SetActive(false);
+        functionsPanel.SetActive(false);
+    }
 
     public void OpenDialog() {
         dialogPanel.SetActive(true);
+        functionsPanel.SetActive(false);
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void CloseDialog() {
         dialogPanel.SetActive(false);
+        functionsPanel.SetActive(false);
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void Back() {
+        functionsPanel.SetActive(false);
+        dialogPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void Upgrade() {
         Debug.Log("Upgrade gewählt");
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void Functions() {
-        Debug.Log("Funktionen gewählt");
+        dialogPanel.SetActive(false);
+        functionsPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void SpawnTower() {
+        Debug.Log("Tower ausgewählt");
+
+        Instantiate(towerPrefab, towerSpawnPoint.position, towerSpawnPoint.rotation);
+
+        EventSystem.current.SetSelectedGameObject(null);
     }
 }
