26117c05bbef26cd5b3ead022320ab9a2946db31
Karsten844 <karsten.bernhardt1@gmx.de>
2026-06-02 00:29:26 +0200

    2 scripts changed

--- Assets/Scripts/ScriptsKarsten/Inventory/Inventory.cs
+++ Assets/Scripts/ScriptsKarsten/Inventory/Inventory.cs
@@ -154,14 +154,16 @@ public class Inventory : MonoBehaviour {
                 hotbarSlots[slot].SetSelected(true);
 
 
-                //Check for item in hotbar then equip the item
-                if (item == assaultRifle) {
-                    ToggleWeaponSelect(assaultRifle);
-                } else if (item == pistol) {
-                    ToggleWeaponSelect(pistol);
-                } else if (hotbarSlots[previousSlot].GetItem() == assaultRifle || hotbarSlots[previousSlot].GetItem() == pistol) {
-                    ToggleWeaponSelect(null);
-                }//Add more else if statements for more options
+                if (previousSlot != -1) {
+                    //Check for item in hotbar then equip the item
+                    if (item == assaultRifle) {
+                        ToggleWeaponSelect(assaultRifle);
+                    } else if (item == pistol) {
+                        ToggleWeaponSelect(pistol);
+                    } else if (hotbarSlots[previousSlot].GetItem() == assaultRifle || hotbarSlots[previousSlot].GetItem() == pistol) {
+                        ToggleWeaponSelect(null);
+                    }//Add more else if statements for more options
+                }
 
 
                 previousSlot = slot;
